3f110e669d38b1932ba02320af633ca813a2fe6e
MultiskillTechnicianHH <multiskill.technicianhh@gmail.com>
2026-09-22 10:53:13 +0530

    Update server.js

--- server.js
+++ server.js
@@ -415,6 +415,6 @@ app.get('/api/owner-information.txt',(req,res)=>{
 app.use(express.static(__dirname));
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
     console.log(`Server running on port ${PORT}`);
 });
